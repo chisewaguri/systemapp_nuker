ac38c76430735ceb6c15c9826318b9235cf9f9d3
ChiseWaguri <lqff.yt@gmail.com>
2025-03-08 18:36:12 +0700

    update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+MODULES_UPDATE_DIR="/data/adb/modules_update/system_app_nuker"
 PERSIST_DIR="/data/adb/system_app_nuker"
-rm -rf "$PERSIST_DIR"
+
+[ -d "$PERSIST_DIR" ] && rm -rf "$PERSIST_DIR"
+[ -d "$MODULES_UPDATE_DIR" ] && rm -rf "$MODULES_UPDATE_DIR"
+
+# EOF
