fc7d72ae13d3b0b965ae6b1c3fa27c6735bbdad6
KOWX712 <leecc0503@gmail.com>
2025-03-08 22:14:35 +0800

    do not remove old app_list
    this might break something thus we store it in persist directory to ensure it won't be changed outside webui

--- customize.sh
+++ customize.sh
@@ -8,6 +8,7 @@ if [ "$MAGISK_VER_CODE" ] || [ "$KSU_MAGIC_MOUNT" ] || [ "$APATCH_BIND_MOUNT" ];
 else
     echo "MAGIC_MOUNT=false" > $PERSIST_DIR/module_system.sh
 fi
+set_perm "$PERSIST_DIR/module_system.sh" 0 2000 0755
 
 mkdir -p "$MODPATH/common"
 mv "$MODPATH/bin/$(getprop ro.product.cpu.abi)/aapt" "$MODPATH/common/aapt"
@@ -16,12 +17,6 @@ set_perm $MODPATH/nuke.sh 0 2000 0755
 rm -rf "$MODPATH/bin"
 
 # Migrate old config
-if [ -d "$PERSIST_DIR" ]; then
-    # Remove applist on installation
-    # Add /my_bigball handling in the future(?)
-    [ -f "$PERSIST_DIR/app_list.json" ] && rm -f "$PERSIST_DIR/app_list.json"
-    # Make sure we don't overwrite previous setup
-    [ -f "$PERSIST_DIR/nuke_list.json" ] && sh "$MODPATH/nuke.sh" skip_symlink &>/dev/null
-fi
+[ -f "$PERSIST_DIR/nuke_list.json" ] && sh "$MODPATH/nuke.sh" skip_symlink &>/dev/null
 
 # EOF
