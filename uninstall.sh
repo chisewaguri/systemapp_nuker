54e2871bf0c7746b2b21f6fb29532d0b892a1bdd
ChiseWaguri <lqff.yt@gmail.com>
2025-03-08 19:25:45 +0700

    script/uninstall: install apps that are uninstalled in uninstallation

--- uninstall.sh
+++ uninstall.sh
@@ -5,4 +5,9 @@ PERSIST_DIR="/data/adb/system_app_nuker"
 [ -d "$PERSIST_DIR" ] && rm -rf "$PERSIST_DIR"
 [ -d "$MODULES_UPDATE_DIR" ] && rm -rf "$MODULES_UPDATE_DIR"
 
+# Install apps that aare uninstalled
+for pkg in $(pm list packages -u | sed 's/package://'); do
+    pm install-existing $pkg && echo "Restored: $pkg"
+done
+
 # EOF
