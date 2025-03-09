f697836d9f675204d4c5f884c0c16086a566e549
ChiseWaguri <188340668+ChiseWaguri@users.noreply.github.com>
2025-03-09 20:35:34 +0700

    scripts/post-fs-data: remove whiteouts in bootloop and tell user what to do
    Co-Authored-By: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -6,11 +6,24 @@ PERSIST_DIR="/data/adb/system_app_nuker"
 BOOTCOUNT=0
 [ -f "$MODDIR/count.sh" ] && . "$MODDIR/count.sh"
 
+if [ $BOOTCOUNT -lt 0 ]; then
+    unzip -o "$MODDIR/dummy.zip" module.prop -d "$MODDIR"
+    BOOTCOUNT=0
+fi
+
 BOOTCOUNT=$(( BOOTCOUNT + 1))
 
 if [ $BOOTCOUNT -gt 1 ]; then
     touch $MODDIR/disable
-    rm "$MODDIR/count.sh"
+    # remove whiteouts
+    for dir in system system_ext vendor product; do
+        rm -rf "$MODULES_UPDATE_DIR/$dir"
+    done
+    # remove applist
+    rm -rf "$PERSIST_DIR/*"
+    string="description=bootloop protection triggered. whiteouts deleted. enable the module to activate."
+    sed -i "s/^description=.*/$string/g" $MODDIR/module.prop
+    echo "BOOTCOUNT=-1" > "$MODDIR/count.sh"
     stop; reboot
 else
     echo "BOOTCOUNT=1" > "$MODDIR/count.sh"
