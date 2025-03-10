dff46ae51a642e49baa83dfb7238f73afe02da02
ChiseWaguri <188340668+ChiseWaguri@users.noreply.github.com>
2025-03-10 20:57:32 +0700

    scripts/uninstall: only reinstall apps in remove list
    this prevent re-installation of apps that are not nuked by this module

--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 MODULES_UPDATE_DIR="/data/adb/modules_update/system_app_nuker"
 PERSIST_DIR="/data/adb/system_app_nuker"
+APP_LIST="$PERSIST_DIR/app_list.json"
+REMOVE_LIST="$PERSIST_DIR/nuke_list.json"
 
 [ -d "$PERSIST_DIR" ] && rm -rf "$PERSIST_DIR"
 [ -d "$MODULES_UPDATE_DIR" ] && rm -rf "$MODULES_UPDATE_DIR"
 
 # Install apps that aare uninstalled
-for pkg in $(pm list packages -u | sed 's/package://'); do
-    pm install-existing $pkg
+for pkg in $(grep -E '"package_name":' "$REMOVE_LIST" | sed 's/.*"package_name": "\(.*\)",/\1/'); do
+    if ! pm list packages | grep -qx "package:$pkg"; then
+        pm install-existing "$pkg"
+    fi
 done
 
 # EOF
