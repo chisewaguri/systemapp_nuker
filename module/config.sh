decac6633f69b23dde03b56a33e551e9a0ab244b
ChiseWaguri <188340668+ChiseWaguri@users.noreply.github.com>
2025-06-10 16:21:42 +0700

    scripts/config: update comments
    this module now will auto-disable use_mountify_script if mountify is already installed and will mount this module

--- module/config.sh
+++ module/config.sh
@@ -1,26 +1,29 @@
 # uninstall fallback if app ain't nuked on late service
-# this executes: pm uninstall -k --user 0
-# only runs if the app still exists after on late-service
+# this runs: pm uninstall -k --user 0
+# only triggers if the app still exists after late-service
 # default is false
 uninstall_fallback=false
 
-# mount the module globally with mountify standalone script
-# requires **TMPFS_XATTR** or **OverlayFS manager**
-# this is auto-enabled during install if the environment supports it
-# DO NOT enable this manually unless you're sure the enviromentment is supported
+# mount the module globally using the standalone mountify script
+# needs either **TMPFS_XATTR** support or the OverlayFS manager
+# auto-enabled during install if the env can handle it
+# if mountify's already installed and will mount this module, this gets auto-disabled
+# DO NOT flip this manually unless you're sure the env supports it
 use_mountify_script=false
 
 # refresh (regenerate) the app list cache every boot
-# this is set to true by default to ensure correct app list when it is changing
+# default is true to make sure app list stays accurate when things change
 refresh_applist=true
 
 # ----
 # ⚠️ DO NOT EDIT BELOW THIS LINE
-# configuration(s) after this are env-specific nor not meant to be edited
-# these values are auto-set and may break things if changed manually
+# config(s) below are env-specific or not meant to be touched
+# these are auto-set and might break stuff if changed manually
 # ----
 
-# mounting system (true = manager is magic mount)
+# mounting system
+# (true = manager is magic mount)
+# (false = manager is overlayfs)
 magic_mount=true
 
 # EOF
