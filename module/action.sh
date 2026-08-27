34dc9fc3587d12e616ddb3dd024d77b9d7b6852d
chise <188340668+chisewaguri@users.noreply.github.com>
2026-08-27 23:31:53 +0700

    fix: skip action ping check

--- module/action.sh
+++ module/action.sh
@@ -43,7 +43,6 @@ get_webui() {
     echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
     mkdir -p "$TMP_DIR" || manual_download "Error: Unable to create temporary directory."
     API="https://api.github.com/repos/KOWX712/KsuWebUIStandalone/releases/latest"
-    ping -c 1 -w 5 raw.githubusercontent.com &>/dev/null || manual_download "Error: Unable to connect to raw.githubusercontent.com, please download manually."
     URL=$(download "$API" | grep -o '"browser_download_url": "[^"]*"' | cut -d '"' -f 4) || manual_download "Error: Unable to get latest version, please download manually."
     download "$URL" > "$APK_PATH" || manual_download "Error: APK download failed, please download manually."
 
